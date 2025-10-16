5048dfe40385add385a772ff6e1dc5081a1d967c
Sourish Dey <satarupa713321@gmail.com>
2025-10-16 08:42:52 +0000

    Try fixing this error: `Console Error: Invalid prop `data-state` supplie

--- src/components/dashboard/user/maintenance-timeline.tsx
+++ src/components/dashboard/user/maintenance-timeline.tsx
@@ -85,8 +85,8 @@ export function MaintenanceTimeline({ vehicle }: { vehicle: Vehicle }) {
           </TableHeader>
           <TableBody>
             {maintenanceHistory.map((item) => (
-              <Collapsible key={item.id} asChild>
-                <React.Fragment>
+              <Collapsible asChild key={item.id}>
+                <>
                   <TableRow>
                     <TableCell>
                       <CollapsibleTrigger asChild>
@@ -116,7 +116,7 @@ export function MaintenanceTimeline({ vehicle }: { vehicle: Vehicle }) {
                           </TableCell>
                       </TableRow>
                   </CollapsibleContent>
-                </React.Fragment>
+                </>
               </Collapsible>
             ))}
           </TableBody>
