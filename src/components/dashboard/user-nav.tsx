cf89e7510f74dee1b1ca56356313594e8074d21e
Sourish Dey <satarupa713321@gmail.com>
2025-10-20 09:36:15 +0000

    Error: Invalid image URL. The URL must be a valid HTTP or HTTPS URL, or

--- src/components/dashboard/user-nav.tsx
+++ src/components/dashboard/user-nav.tsx
@@ -27,7 +27,7 @@ export function UserNav() {
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" className="relative h-9 w-9 rounded-full">
           <Avatar className="h-9 w-9">
-            <AvatarImage src={user.avatarUrl} alt={`@${user.name}`} />
+            {user.avatarUrl && <AvatarImage src={user.avatarUrl} alt={`@${user.name}`} />}
             <AvatarFallback>{userInitials}</AvatarFallback>
           </Avatar>
         </Button>
