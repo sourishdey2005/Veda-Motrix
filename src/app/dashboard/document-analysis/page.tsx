9bd0ad5140db6d51ef3ff0a9da20e978d1987495
Sourish Dey <satarupa713321@gmail.com>
2025-10-20 07:01:34 +0000

    Error: ./src/app/dashboard/document-analysis/page.tsx:4:1 Export [32mDo

--- src/app/dashboard/document-analysis/page.tsx
+++
@@ -1,8 +0,0 @@
-
-"use client";
-
-import { DocumentAnalysisView } from "@/components/dashboard/manager/document-analysis-view";
-
-export default function DocumentAnalysisPage() {
-  return <DocumentAnalysisView />;
-}
